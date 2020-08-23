45b83cae38512cc6eaeeef14f7c680ba34b538a2
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-08-23 22:47:17 +0530

    type

--- bin/work.dart
+++ bin/work.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
 import 'package:work/work.dart' as work;
 
 void main() {
-  var firsName = 'Mohammed';
-  String lastName = 'Rashid';
-  print(lastName + ' ' + firsName);
+  // var firsName = 'Mohammed';
+  // String lastName = 'Rashid';
+  // print(lastName + ' ' + firsName);
+
+//type
+
+  stdout.writeln('what is your name');
+  String name = stdin.readLineSync();
+
+  print(name);
 }
